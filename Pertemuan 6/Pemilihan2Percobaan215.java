5f25b7efcacd74249d7a2e22dd7928ff8939f04a
LuthfiyyahA <luthfiyyahadzka@gmail.com>
2023-10-03 21:35:25 +0700

    update

--- Pertemuan 6/Pemilihan2Percobaan215.java
+++ Pertemuan 6/Pemilihan2Percobaan215.java
@@ -13,10 +13,15 @@ public class Pemilihan2Percobaan215 {
         totalSudut = sudut1+sudut2+sudut3;
 
         if (totalSudut == 180) {
-            if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-                System.out.println("Segitiga tersebut adalah segitiga siku-siku"); 
-            else 
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
+            if (((sudut1 == sudut2) && (sudut3 != sudut1)) || ((sudut1 == sudut3) && (sudut2) != (sudut1)) || ((sudut2 == sudut3) && (sudut1 != sudut2))) {
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            } else {
+                if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) {
+                    System.out.println("Segitiga tersebut adalah segitiga siku-siku");
+                } else {
+                    System.out.println("Segitiga tersebut adalah segitiga sembarang");
+                }
+            }
         } else {
             System.out.println("Bukan Segitiga");
         }
